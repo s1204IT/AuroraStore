769870d21171b500ea33b082d95c5f23c7cccc7e
Syuugo <pub@s1204.me>
2023-03-09 13:22:02 +0900

    動作しない問題を修正

---
+++ app/src/main/java/com/afwsamples/testdpc/DeviceAdminReceiver.java
@@ -0,0 +1,4 @@
+package com.testdpc.afwsamples;
+
+public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
+}
--- app/src/main/java/com/aurora/store/data/receiver/AdminReceiver.kt
+++
@@ -1,6 +0,0 @@
-package com.aurora.store.data.receiver
-
-import android.app.admin.DeviceAdminReceiver
-
-class AdminReceiver : DeviceAdminReceiver() {
-}
